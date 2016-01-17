2821b41806a90e3f719a726cc42c04a959ffa8cd
Luke Fair <info@nonlocalhq.com>
2016-01-17 18:13:19 -0500

    Browsers talking to eachother

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,9 @@ var socket = io();
 socket.on('connect', function () {
 	console.log('Connected to socket.io server!');
 });
+
+// checking for message from server, displaying once it hits
+socket.on('message', function (data) {
+	console.log('New message:');
+	console.log(data.text);
+})
--- server.js
+++ server.js
@@ -7,8 +7,21 @@ var io = require('socket.io')(http);
 app.use(express.static(__dirname + '/public'));
 
 // io.on listens for events
-io.on('connection', function () {
+io.on('connection', function (socket) {
 	console.log('User connected via socket.io!');
+
+	socket.on('message', function (message) {
+		console.log('Message received: ' + message.text);
+
+		// use io.broadcast to emit even to sender
+		// broadcast sends to everyone but person who sent it
+		socket.broadcast.emit('message', message);
+	});
+
+	// emit takes arbitrary event name + data
+	socket.emit('message', {
+		text: 'Welcome to the chat application!'
+	});
 });
 
 http.listen(PORT, function () {
