1d1402e4e7c71cb1cf5457de6d5e387e164b8501
Luke Fair <info@nonlocalhq.com>
2016-01-17 19:31:37 -0500

    Messages sending

--- public/js/app.js
+++ public/js/app.js
@@ -8,4 +8,40 @@ socket.on('connect', function () {
 socket.on('message', function (data) {
 	console.log('New message:');
 	console.log(data.text);
-})
+	ui.interactiveModule.postMessage(data);
+});
+
+var ui = {};
+
+ui.interactiveModule = {
+	cacheDom: function () {
+		this.$form = $('#message-form');
+		this.$message = $('#message');
+		this.$chat = $('#chat');
+	},
+	bindEvents: function () {
+		this.submitMessage();
+	},
+	init: function () {
+		this.cacheDom();
+		this.bindEvents();
+	},
+	submitMessage: function () {
+		var self = this;
+		this.$form.on('submit', function (event) {
+			event.preventDefault();
+
+			socket.emit('message', {
+				text: self.$message.val()
+			});
+
+			self.$message.val('');
+		});
+	},
+	postMessage: function (message) {
+		var self = this;
+		this.$chat.append('<li>' + message.text + '</li>');
+	}
+}
+
+ui.interactiveModule.init();
